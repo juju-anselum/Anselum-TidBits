95f1f458d13b7ccd66eb8ab06b72ae20a32681cc
juju-anselum <anselumjuju@gmail.com>
2024-09-08 16:53:43 +0530

    ActionBar Removed

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,1 @@
-export { default as Navbar } from "./Navbar";
-export { default as ActionBar } from "./ActionBar";
 export { default as Button } from "./ui/Button"
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import { Header } from "./containers";
+import { Blogs } from "./containers";
 
-export default function Home() {
+
+
+export default async function Home() {
   return (
-    <div className="w-full max-w-[100vw] mx-auto px-5 2xl:px-0">
-      <Header />
-    </div>
+    <>
+      <div className='w-full flex flex-col items-stretch md:gap-y-3'>
+        <h1 className='py-3 md:py-0 text-[10.5vw] lg:text-[12vw] 2xl:text-[160px] text-center font-normal font-blackOpsOne text-primary-background text-nowrap'>Anselum Tidbits</h1>
+        <div className='w-full h-[1px] bg-gray-300' />
+      </div>
+      <Blogs />
+    </>
   );
 }
