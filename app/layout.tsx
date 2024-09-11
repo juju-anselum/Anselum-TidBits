bfd202d84840c4b78bfdd8a80741e1041fac2a88
juju-anselum <anselumjuju@gmail.com>
2024-09-11 23:42:17 +0530

    blogBanner added

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,19 +21,20 @@ const inter = localFont({
 export const metadata: Metadata = {
   title: "Anselum TidBits",
   description: "Personal Blog app about Tech, Programming, Web Development, and more.",
+  keywords: ['Nextjs', 'anselum-tidbits', 'anselum', 'tidbits', 'blog', 'typescript', 'tailwind', 'material-ui', 'sanity'],
   openGraph: {
     url: "https://anselum-tidbits.vercel.app/",
     type: "website",
     title: "Anselum TidBits",
     description: "Personal Blog app about Tech, Programming, Web Development, and more.",
-    images: ['./assets/img/blogBanner.png'],
+    images: ['https://anselum-tidbits.vercel.app/blogBanner.png'],
   },
   twitter: {
     card: "summary_large_image",
     site: "https://anselum-tidbits.vercel.app/",
     title: "Anselum TidBits",
     description: "Personal Blog app about Tech, Programming, Web Development, and more.",
-    images: ['./assets/img/blogBanner.png'],
+    images: ['https://anselum-tidbits.vercel.app/blogBanner.png'],
   },
 };
 
